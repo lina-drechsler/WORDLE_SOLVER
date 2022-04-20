2a7227ffef7d64ad4382353a5d52de3d78fdfc30
lina drechsler <87331786+lina-drechsler@users.noreply.github.com>
2022-04-20 17:57:53 +0200

    Update length_visualization.py
    correctly produces the bar chart

--- length_visualization.py
+++ length_visualization.py
@@ -2,11 +2,13 @@
 #and create a visualization of which n-lettered words are most abundant
 
 #imports
-from matplotlib.pyplot import ply
 import matplotlib.pyplot as plt
 
 #clean up the data after scaraped (ignore dashes, spaces etc)
-
+words = []
+with open('words_alpha.txt', 'r') as f:
+    for word in f.readlines():
+        words.append(word.rstrip())
 
 #count the lenghts using a dictionary
 d = {}
@@ -19,7 +21,7 @@ length, count = zip(*d.items())
 
 #plot the n-lettered words and their counts
 plt.bar(length, count)
-plt.xticks(range(1, max(lengths)+1))
+plt.xticks(range(1, max(length)+1))
 plt.xlabel('word lengths (in letters)')
 plt.ylabel('word count')
 plt.title('quanitity of n-lettered words in the dictionary')
