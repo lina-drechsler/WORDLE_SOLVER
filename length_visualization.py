04f4156858da31a2f549ec71d2edf595d62f23c7
mdisant <mdisant@umich.edu>
2022-04-21 09:35:30 +0200

    Update length_visualization.py
    Updated the file path on line 9

--- length_visualization.py
+++ length_visualization.py
@@ -1,30 +1,29 @@
-#The purpose of this file is to analyze the data (dictionary words)
-#and create a visualization of which n-lettered words are most abundant
+# The purpose of this file is to analyze the data (dictionary words)
+# and create a visualization of which n-lettered words are most abundant
 
-#imports
+# imports
 import matplotlib.pyplot as plt
 
-#clean up the data after scaraped (ignore dashes, spaces etc)
+# clean up the data after scaraped (ignore dashes, spaces etc)
 words = []
-with open('words_alpha.txt', 'r') as f:
+with open("./data/words_alpha.txt", "r") as f:
     for word in f.readlines():
         words.append(word.rstrip())
 
-#count the lenghts using a dictionary
+# count the lenghts using a dictionary
 d = {}
 for w in words:
     l = len(w)
     d[l] = d.get(l, 0) + 1
 
-#retrieve into from the dictionary created
+# retrieve into from the dictionary created
 length, count = zip(*d.items())
 
-#plot the n-lettered words and their counts
+# plot the n-lettered words and their counts
 plt.bar(length, count)
-plt.xticks(range(1, max(length)+1))
-plt.xlabel('word lengths (in letters)')
-plt.ylabel('word count')
-plt.title('quanitity of n-lettered words in the dictionary')
+plt.xticks(range(1, max(length) + 1))
+plt.xlabel("word lengths (in letters)")
+plt.ylabel("word count")
+plt.title("quanitity of n-lettered words in the dictionary")
 
 plt.show()
-
