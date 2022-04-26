1598f48279d51abbdb6d9cb8b3735288d91afd25
mdisant <mdisant@umich.edu>
2022-04-26 14:00:20 +0200

    Updated data collection/sorting

--- data_maker.py
+++ data_maker.py
@@ -1,13 +1,7 @@
-############################    ############################
-############################    ############################
-### DO NOT RUN THIS FILE ###    ### DO NOT RUN THIS FILE ###
-############################    ############################
-############################    ############################
-
 # Takes the data from words_alpha.txt and organizes the words into .txt files based on word length
 
 # Step 1: Get all the data into a dictionary
-file = "./data/words_alpha.txt"
+file = "./data/words_alpha1.txt"
 
 all_words = {}
 with open(file, "r") as f:
@@ -20,23 +14,25 @@ with open(file, "r") as f:
             all_words[word_len] = []
         all_words[word_len].append(word)
 
-# Step 2: Only include word lengths between 5 and 14 (inclusive)
+# Step 2: Only include word lengths between 3 and 10 letters (inclusive)
 keys = list(all_words.keys())
 for key in keys:
-    if key < 5 or key > 14:
+    if key < 3 or key > 10:
         all_words.pop(key)
     else:
         continue
 
 # Step 3: Write the words of each length to their respective .txt files
+import os
+
 for word_len, words in sorted(all_words.items()):
     path = f"./data/{word_len}.txt"
-    with open(path, "a") as f:
+    with open(path, "w") as f:
         for word in words:
             f.write(f"{word}\n")
 
-############################    ############################
-############################    ############################
-### DO NOT RUN THIS FILE ###    ### DO NOT RUN THIS FILE ###
-############################    ############################
-############################    ############################
+# Step 4: Write all the words to a JSON File
+import json
+
+with open("words.json", "w") as json_file:
+    json.dump(all_words, json_file)
